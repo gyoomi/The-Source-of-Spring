97c55247f0bbb8e3b03c98cfc2627ab3d1c47014
leon <gyoomi0709@foxmail.com>
2019-04-17 23:39:34 +0800

    Initial commit

---
+++ src/main/java/com/gyoomi/source/SpringSourceApplication.java
@@ -0,0 +1,18 @@
+package com.gyoomi.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/4/17 23:36
+ */
+@SpringBootApplication
+public class SpringSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class, args);
+    }
+}
